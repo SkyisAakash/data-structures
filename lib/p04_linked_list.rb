5cea58180565a1791e7dbfc19d5e5dd37e8a988f
Aakash Sarang <sarangaakash@gmail.com>
2018-07-03 22:47:53 -0700

    Finish string palindrom

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Node
   attr_accessor :key, :val, :next, :prev
 
@@ -13,49 +15,94 @@ class Node
   end
 
   def remove
-    # optional but useful, connects previous node to next node
+    # optional but useful, connects previous link to next link
     # and removes self from list.
   end
 end
 
 class LinkedList
+
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
-    each_with_index { |node, j| return node if i == j }
+    each_with_index { |link, j| return link if i == j }
     nil
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    node = find_node(key)
+    node.nil? ? nil : node.val
   end
 
+  def find_node(key)
+    found = false
+    start = first
+    until found
+      break if start == @tail
+      if start.key == key
+        return start
+      else
+        start = start.next
+      end
+    end
+    nil
+  end
+
+
   def include?(key)
+    !find_node(key).nil?
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+    node.next, node.prev, = @tail, @tail.prev
+    @tail.prev.next = node
+    @tail.prev = node
   end
 
   def update(key, val)
+    # debugger
+    node = find_node(key)
+    node.nil? ? nil : node.val = val
   end
 
   def remove(key)
+    node = find_node(key)
+    node.prev.next = node.next
+    node.next.prev = node.prev
   end
 
-  def each
+  def each(&prc)
+    start = first
+    each_arr = []
+    while true
+      return each_arr if start == @tail
+      each_arr << prc.call(start)
+      start = start.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
--- lib/p07_practical.rb
+++ lib/p07_practical.rb
@@ -1,5 +1,22 @@
 require_relative 'p05_hash_map'
 
 def can_string_be_palindrome?(string) 
-  
+  memory = HashMap.new()
+  string.chars.each do |char|
+    current = memory.get(char)
+    if current
+        current += 1
+    else
+        current = 1
+    end
+    memory.set(char, current)
+  end
+  counter = 0
+  string.chars.each do |char|
+    if memory.get(char) == 1
+        counter += 1
+    end
+    return false if counter > 1
+    end
+    return true
 end
